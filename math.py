a706328634c10c8fc1a7e423d6de13adf314824d
Emma-Nangida <emmanangida@gmail.com>
2023-03-29 22:58:52 +0300

    first commmit

---
+++ math.py
@@ -0,0 +1,43 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract(d,b):
+    answer=d-b
+    return answer
+
+def subtract(e,f):
+    answer=e-f
+    return answer
+
+def subtract(r,z):
+    answer=r-z
+    return answer
+
+def divide(m,y):
+    answer=m/y
+    return answer
+
+def divide(w,a):
+    answer=w/a
+    return answer
+
+def divide(s,x):
+    answer=s/x
+    return answer
+
+def multiply(q,w):
+    answer=q*w
+    return answer
+
+def multiply(d,j):
+    answer=d*j
+    return answer
+def multiply(i,p):
+    answer=i*p
+    return answer
+
+def remainder(c,d):
+    answer=c%d
+    return answer
+
